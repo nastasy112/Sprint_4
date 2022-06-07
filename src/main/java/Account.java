7e91ff3751ad1986a38bb6c647feab81eff8638d
Anastasiia Sytova <Sytova.Anastasiia@yandex.ru>
2022-06-07 09:56:50 +0300

    Fix 'if' in Account.java

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -13,7 +13,7 @@ public class Account {
          */
 //        В классе Account задай проверки: длина, наличие пробела в начале или конце строки. Используй методы класса
 
-        if((!name.isEmpty())&(name.length() >= 3)&(name.length() <= 19)&(name.contains(" "))&(!name.startsWith(" "))&(!name.endsWith(" "))){
+        if((!name.isEmpty()) & (name.length() >= 3) & (name.length() <= 19) & (name.contains(" ")) & (name.length() == (name.replace(" ", "").length() +1)) & (!name.endsWith(" ")) & (!name.startsWith(" "))){
             return true;
         } else
             return false;
